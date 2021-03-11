c192e95b3f49f81a359df3dbc19770c0f47e3f87
David Schmoelz <david.schmoelz@tum.de>
2021-03-11 17:23:31 +0100

    erste unitTests fuer Meshes

--- vmtool/__init__.py
+++ vmtool/__init__.py
@@ -2,3 +2,8 @@ from .Node import *
 from .Mesh import Mesh
 from .ForwardMapping import *
 from .ShapeFunctions import *
+
+def test():
+    import unittest
+    suite = unittest.TestLoader().discover('.', pattern='test*')
+    unittest.TextTestRunner(verbosity=2).run(suite)
